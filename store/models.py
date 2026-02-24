2fe8f63dedde989959a1283f5d69569aa89edf57
parahinogjessie <parahinogjessie.com>
2026-02-24 23:44:16 +0800

    Your commit message

--- store/models.py
+++ store/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-#for Product
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='products/', default='products/default.png')
 
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Product
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html',{'products': products})
+
+#Handles the add to cart
+def add_to_cart(request, product_id):
+   cart = request.session.get('cart', [])
+   cart.append(product_id)
+   request.session['cart'] = cart   
+   return redirect('home')
+
+def cart(request):
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(id__in=cart)
+    return render(request, 'store/cart.html', {'products': products})
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', [])
+    if product_id in cart:
+        cart.remove(product_id)
+        request.session['cart'] = cart
+        return redirect('cart')
+    
